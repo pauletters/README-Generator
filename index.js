e211fd65157986c91dd2cd304e9552922b8e5bea
pauletters <140007528+pauletters@users.noreply.github.com>
2024-08-24 12:13:16 -0400

    initial-commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+// TODO: Create an array of questions for user input
+const questions = ['title', 'description', 'installation', 'usage', 'contributing', 'tests'];
+
+inquirer.prompt(questions).then((answers) => {
+    const userTemplate = fillTemplate(answers);
+    writeFile(userTemplate);
+});
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
